caae0ff229e95b766d219392e5a1b80d5e34c3cb
Samar Omar <samaromm98@gmail.com>
2020-02-14 12:53:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// Write your code here
-
-class Dog{
-  constructor(name,breed){
-    this.name=name
-    this.breed=breed
-  }
+class Breakfast{
+  consturctor
 }
-
-let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
-let bigFluffyDog2 = new Dog("Woody", "labrador");
